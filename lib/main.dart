385d8aec56d5e3133ebde1e2d490610f095d1565
Maryam-rgb <Maryamadejeylani@gmail.com>
2026-08-16 22:46:09 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'app/campus_go_app.dart';
 import 'core/config/supabase_config.dart';
-import 'features/bookings/controllers/booking_controller.dart';
-import 'features/bookings/services/booking_service.dart';
-import 'features/bookings/views/bookings_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,20 +12,5 @@ Future<void> main() async {
     publishableKey: SupabaseConfig.publishableKey,
   );
 
-  await Supabase.instance.client.auth.signInWithPassword(
-    email: 'B01@student.unimy.my',
-    password: 'newpassword123',
-  );
-
-  final userId = Supabase.instance.client.auth.currentUser!.id;
-
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => BookingController(
-        BookingService(Supabase.instance.client),
-        userId,
-      ),
-      child: const MaterialApp(home: BookingsScreen()),
-    ),
-  );
+  runApp(const CampusGoApp());
 }
